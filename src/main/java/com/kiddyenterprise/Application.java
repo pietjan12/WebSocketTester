135e8b71359bdd415083bf5e919eb842064e2987
nick van der burgt <n.vanderburgt@student.fontys.nl>
2019-01-08 09:24:46 +0100

    ya yeet

---
+++ src/main/java/com/kiddyenterprise/Application.java
@@ -0,0 +1,36 @@
+package com.kiddyenterprise;
+
+import com.kiddyenterprise.Config.MyStompSessionHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Application {
+
+    private static String URL = "ws://localhost:8080/websocket";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        testWebSocket();
+    }
+
+    private static void testWebSocket() {
+        WebSocketClient client = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(client);
+
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSessionHandler sessionHandler = new MyStompSessionHandler();
+        stompClient.connect(URL, sessionHandler);
+
+        new Scanner(System.in).nextLine(); // Don't close immediately.
+    }
+}
